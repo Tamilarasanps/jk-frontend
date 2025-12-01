c21ce5ff28fa6d963cdc9b787c3e557d7f11d9e2
Tamilarasanps <tamilarasantamil2530@gmail.com>
2025-12-01 13:05:16 +0530

    cal

--- src/components/Client/Pricing/PricePdf.jsx
+++ src/components/Client/Pricing/PricePdf.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import pricePdf from "../../../../public/pricepdf.pdf";
+import ConstructionCalculator from "./ConstructionCalculator";
 
 export default function PricePdf() {
   return (
-    <div className="w-full h-screen p-2">
-      <iframe
-        src={pricePdf}
-        className="w-full h-full rounded-lg shadow"
-        style={{ border: "none" }}
-        title="Price PDF"
-      />
+    <div className="">
+      <div className="w-full h-screen p-2">
+        <iframe
+          src={pricePdf}
+          className="w-full h-full rounded-lg shadow"
+          style={{ border: "none" }}
+          title="Price PDF"
+        />
+      </div>
+      <ConstructionCalculator />
     </div>
   );
 }
